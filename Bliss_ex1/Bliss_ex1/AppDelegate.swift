28c4884a087de33338aff6d9ce733576b9247c04
FilipeSanto00 <91618412+FilipeSanto00@users.noreply.github.com>
2021-10-07 13:02:05 +0100

    commit

--- Bliss_ex1/Bliss_ex1/AppDelegate.swift
+++ Bliss_ex1/Bliss_ex1/AppDelegate.swift
@@ -9,10 +9,11 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    let apiClient = ApiClient()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        apiClient.getLatest()
         return true
     }
 
